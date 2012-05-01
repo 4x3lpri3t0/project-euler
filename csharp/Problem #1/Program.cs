8957e4c877e49694c125ebb8f4338c84acbceccc
Axel Prieto <prietoaxel@gmail.com>
2012-04-30 23:55:37 -0300

    First check-in.

---
+++ csharp/Problem #1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace euler1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int acum = 0;
+            for (int i = 1; i < 1000; i++) {
+                if (i%3 == 0 || i%5 == 0)
+                {
+                    acum += i;
+                    Console.WriteLine("Current num: " + i + " Acumulator: " + acum);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
